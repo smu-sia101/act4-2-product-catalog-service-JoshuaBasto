8228244fe93c22c1f555e340a6054c1004ead93d
JoshuaBasto <JoshuaBasto@SMU-0045>
2025-04-04 17:57:25 +0800

    kk

---
+++ ProductCatalog/ProductCatalog/Models/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ProductCatalog.Models
+{
+    public class ProductModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string ProductName { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+    }
+}
---
+++ ProductCatalog/ProductCatalog/Program.cs
@@ -0,0 +1,39 @@
+using ProductCatalog.Models;
+using ProductCatalog.Services;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<ProductService>();
+builder.Services.Configure<ProductDatabaseSettings>(
+    builder.Configuration.GetSection("ProductDatabase"));
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+
